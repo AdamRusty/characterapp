9b395497a2d3b8a53f9694bee52313923f5f8d84
AdamRusty <adam.rusty998@gmail.com>
2017-12-13 21:21:37 +0000

    gulp added.

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# characterapp
+# Characterapp
 An app which allows people to view and edit character data.
+
+## How to install
+
+To clone:
+
+```
+git clone https://github.com/AdamRusty/characterapp.git
+```
+
+Once cloned to install packagess locally run:
+
+```
+npm install
+```
+
+That Should be all for basic setup unless I add something else.
+
+## Node Server setup.
---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+"use strict";
+
+/* Dependencies  */
+var gulp = require('gulp'),
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps'),
+    del = require('del');
+
+
+/* Concat Js */
+gulp.task("concatJs", function() {
+    return gulp.src([
+        /* add js files to include.*/
+        'js/script.js'
+    ])
+    .pipe(maps.init())
+    .pipe(concat('app.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('js'));
+});
+
+/* Js Minify */
+/* Must concat before minify */
+gulp.task("minifyJs", ["concatJs"], function() {
+    return gulp.src("js/app.js")
+        .pipe(uglify())
+        .pipe(rename('app.min.js'))
+        .pipe(gulp.dest('public/js'));
+});
+
+/* Compile Sass */
+gulp.task('compileSass', function() {
+    return gulp.src("scss/app.scss")
+        .pipe(maps.init())
+        .pipe(sass())
+        .pipe(maps.write('./'))
+        .pipe(gulp.dest('public/css'));
+});
+
+/* gulp clean */
+/* Delete dist */
+gulp.task('clean', function () {
+    del('dist');
+});
+
+
+/* Auto sass and Js compile */
+gulp.task('watch', function() {
+    gulp.watch('scss/**/*.scss', ['compileSass']);
+    gulp.watch('js/main.js', ['concatJs']);
+})
+
+gulp.task('data', function() {
+    return gulp.src('js/data/**')
+        .pipe(gulp.dest('public/data'));
+})
+
+/* gulp prod */
+/* Moves compiled files to production. */
+gulp.task('prod', ['minifyJs', 'compileSass'], function () {
+    return gulp.src([
+        'public/css/app.css', 
+        'public/js/app.min.js',
+        'index.html',
+        'img/**', 
+        'fonts/**'
+    ],                    
+/* Retain file structure */
+    { base: './' })
+    .pipe(gulp.dest('dist'))
+})
+
+/* gulp build */
+/* Backup if gulp cannot be used. */
+gulp.task("build", ['minifyJs', 'compileSass', 'data']);
+
+/* gulp */
+gulp.task("default", ["build"]);
