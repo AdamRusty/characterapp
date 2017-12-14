bac96e6d8597f46a063e8c21aa26d21a041a831c
AdamRusty <adam.rusty998@gmail.com>
2017-12-14 21:57:52 +0000

    local build server opens to index.

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
-var http = require('http');
-var opn = require('opn');
+let http = require('http'),
+    opn = require('opn'),
+    express = require('express'),
+    path = require('path'),
+    router = require('router'),
+    app = express(),
+    user = express(),
+    admin = express();
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "/build/templates"));
 
 
 opn('localhost:3000/', {app: 'firefox'});
 
-http.createServer(function(req, res) {
-    console.log("Booting up");
-    res.write("Page hit");
-    res.end();
-}).listen(3000);
+
+app.get("/", (req, res) => {
+    console.log('here');
+    res.render("index", {title: 'D&Dapp', message: "First pug bootup successful! :D" });
+});
+
+
+app.listen(3000, () => {
+    console.log("locate homepage.");
+});
+
